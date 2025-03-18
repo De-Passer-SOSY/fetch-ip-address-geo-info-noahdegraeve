c3ef8c92d9ee17e569ef3253b6796caef7ea12e3
noahdegraeve <noah.degraeve@leerling.de-passer.be>
2025-03-18 11:43:30 +0100

    geprobeerd het te laten werken

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    document.querySelector("#getIp").addEventListener("click", fetchIp);
+    document.querySelector("#getlocation").addEventListener("click", fetchlocation);
+    console.log("de website is ingeladen");
+}
+
+async function fetchIp() {
+    try {
+        let response = await fetch("https://api.ipify.org/?format=json");
+        let data = await response.json();
+        displayIp(data);
+    } catch (error) {
+        console.log("fout bij het ophalen van de API voor het ip adres", error);
+    }
+}
+
+async function fetchlocation() {
+    try {
+        let response = await fetch("https://ipinfo.io/195.130.157.67/geo");
+        let data = await response.json()
+        console.log(data);
+        displayLocation(data);
+    } catch (error) {
+        console.log("fout bij het ophalen van de API voor de locatie", error);
+    }
+}
+
+function displayIp(data) {
+    const container = document.querySelector("#ipContainer");
+    container.innerHTML = data.ip;
+}
+
+function displayLocation(data) {
+    const container = document.querySelector("#locationContainer");
+    container.innerHTML = data.city;
+
+}
